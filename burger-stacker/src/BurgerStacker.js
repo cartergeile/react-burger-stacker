4ca40211325fda8dec71a9c629708ef3559ba471
Carter Geile <cartergeile@l.local>
2023-02-07 09:42:30 -0600

    changed all files from class based components to function components

--- burger-stacker/src/BurgerStacker.js
+++ burger-stacker/src/BurgerStacker.js
@@ -1,73 +1,69 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import IngredientList from './IngredientList'
 import BurgerPane from './BurgerPane'
-
-export default class BurgerStacker extends Component {
-  state = {
-    ingredients: [
-      {name: 'Kaiser Bun', color: 'saddlebrown'},
-      {name: 'Sesame Bun', color: 'sandybrown'},
-      {name: 'Gluten Free Bun', color: 'peru'},
-      {name: 'Lettuce Wrap', color: 'olivedrab'},
-      {name: 'Beef Patty', color: '#3F250B'},
-      {name: 'Soy Patty', color: '#3F250B'},
-      {name: 'Black Bean Patty', color: '#3F250B'},
-      {name: 'Chicken Patty', color: 'burlywood'},
-      {name: 'Lettuce', color: 'lawngreen'},
-      {name: 'Tomato', color: 'tomato'},
-      {name: 'Bacon', color: 'maroon'},
-      {name: 'Onion', color: 'lightyellow'}
-    ],
-    burgerIngredients: []
-  }
+// need to get rid of class syntax, in lieu of const decloration
+// look thru component and get rid of any of these:
+  // any use of the word 'this'
+  // and use of setState
+const BurgerStacker = () => {
+  const ingredients = [
+    {name: 'Kaiser Bun', color: 'saddlebrown'},
+    {name: 'Sesame Bun', color: 'sandybrown'},
+    {name: 'Gluten Free Bun', color: 'peru'},
+    {name: 'Lettuce Wrap', color: 'olivedrab'},
+    {name: 'Beef Patty', color: '#3F250B'},
+    {name: 'Soy Patty', color: '#3F250B'},
+    {name: 'Black Bean Patty', color: '#3F250B'},
+    {name: 'Chicken Patty', color: 'burlywood'},
+    {name: 'Lettuce', color: 'lawngreen'},
+    {name: 'Tomato', color: 'tomato'},
+    {name: 'Bacon', color: 'maroon'},
+    {name: 'Onion', color: 'lightyellow'}
+  ]
+  const [burgerIngredients, setBurgerIngredients] = useState ([]) 
+  
 
   // function adds items to burgerIngredients array in state
-  addToStack = (e) => {
+  const addToStack = (e) => {
     const ingName = e.target.innerText
     const ingColor = e.target.style.backgroundColor
 
     console.log(`clicked on ${ingName} and it is ${ingColor}`)
 
-    this.setState({
-      burgerIngredients: [
-        { name: ingName, color: ingColor },
-        ...this.state.burgerIngredients
-      ]
-    })
+    setBurgerIngredients(
+      [{ name: ingName, color: ingColor }, ...burgerIngredients]
+    )
   }
 
   // function to remove individual items from burger
-  removeFromStack = (e) => {
+  const removeFromStack = (e) => {
     const clickIndex = e.target.id
-    const currBurger = this.state.burgerIngredients.slice()
+    const currBurger = burgerIngredients.slice()
     currBurger.splice(clickIndex, 1)
-    this.setState({
-      burgerIngredients: currBurger
-    })
+    setBurgerIngredients(currBurger)
   }
 
-  clearBurger = () => {
-    this.setState({
-      burgerIngredients: []
-    })
+  const clearBurger = () => {
+    setBurgerIngredients([])
   }
 
-  render () {
-    return (
-      <div>
-        <h1>Burger Stacker</h1>
-        <div className='panes'>
-          <IngredientList
-            ingredients={this.state.ingredients}
-            add={this.addToStack} 
-            />
-          <BurgerPane 
-            ingredients={this.state.burgerIngredients}
-            clear={this.clearBurger}
-            remove={this.removeFromStack}
-            />
-        </div>
+// rid of render, return only one thing
+  return (
+    <div>
+      <h1>Burger Stacker</h1>
+      <div className='panes'>
+        <IngredientList
+          ingredients={ingredients}
+          add={addToStack} 
+          />
+        <BurgerPane 
+          ingredients={burgerIngredients}
+          clear={clearBurger}
+          remove={removeFromStack}
+          />
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default BurgerStacker
